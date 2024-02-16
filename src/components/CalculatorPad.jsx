f06c13abf6a3ed9bb75b41eee3f61013b39426c2
ITsPorky <itsporky16@gmail.com>
2024-02-16 20:55:48 +1000

    Updated button padding and added hover color

--- src/components/CalculatorPad.jsx
+++ src/components/CalculatorPad.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import CalculatorButton from './CalculatorButton';
+import React from "react";
+import CalculatorButton from "./CalculatorButton";
 
 const CalculatorPad = (props) => {
-    return ( 
-        <div id='CalculatorPad' className='grid grid-cols-3 p-6'>
-            <CalculatorButton className='btn' value={'clr'} callback={props.clear} />
-            <CalculatorButton className='btn' value={'- / +'} callback={props.negative} />
-            <CalculatorButton className='btn' value={'*'} callback={props.operator} />
-            <CalculatorButton className='btn' value={'/'} callback={props.operator} />
-            <CalculatorButton className='btn' value={'+'} callback={props.operator} />
-            <CalculatorButton className='btn' value={'-'} callback={props.operator} />
-            <CalculatorButton className='btn' value={9} callback={props.input} />
-            <CalculatorButton className='btn' value={8} callback={props.input} />
-            <CalculatorButton className='btn' value={7} callback={props.input} />
-            <CalculatorButton className='btn' value={6} callback={props.input} />
-            <CalculatorButton className='btn' value={5} callback={props.input} />
-            <CalculatorButton className='btn' value={4} callback={props.input} />
-            <CalculatorButton className='btn' value={3} callback={props.input} />
-            <CalculatorButton className='btn' value={2} callback={props.input} />
-            <CalculatorButton className='btn' value={1} callback={props.input} />
-            <CalculatorButton className='btn' value={'.'} callback={props.input} />
-            <CalculatorButton className='btn' value={0} callback={props.input} />
-            <CalculatorButton className='btn' value={'='} callback={props.calculate} />
-        </div>
-    );
-}
- 
+  return (
+    <div id="CalculatorPad" className="grid grid-cols-3 py-6">
+      <CalculatorButton className="btn" value={"clr"} callback={props.clear} />
+      <CalculatorButton
+        className="btn"
+        value={"- / +"}
+        callback={props.negative}
+      />
+      <CalculatorButton className="btn" value={"*"} callback={props.operator} />
+      <CalculatorButton className="btn" value={"/"} callback={props.operator} />
+      <CalculatorButton className="btn" value={"+"} callback={props.operator} />
+      <CalculatorButton className="btn" value={"-"} callback={props.operator} />
+      <CalculatorButton className="btn" value={9} callback={props.input} />
+      <CalculatorButton className="btn" value={8} callback={props.input} />
+      <CalculatorButton className="btn" value={7} callback={props.input} />
+      <CalculatorButton className="btn" value={6} callback={props.input} />
+      <CalculatorButton className="btn" value={5} callback={props.input} />
+      <CalculatorButton className="btn" value={4} callback={props.input} />
+      <CalculatorButton className="btn" value={3} callback={props.input} />
+      <CalculatorButton className="btn" value={2} callback={props.input} />
+      <CalculatorButton className="btn" value={1} callback={props.input} />
+      <CalculatorButton className="btn" value={"."} callback={props.input} />
+      <CalculatorButton className="btn" value={0} callback={props.input} />
+      <CalculatorButton
+        className="btn"
+        value={"="}
+        callback={props.calculate}
+      />
+    </div>
+  );
+};
+
 export default CalculatorPad;
